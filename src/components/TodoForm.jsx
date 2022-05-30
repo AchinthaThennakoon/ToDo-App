22366cc3c110f7bb1ad2030cdd7dc8c671f7af43
achintha <achinthathennakoon97@gmail.com>
2022-05-30 17:08:32 +0530

    added input & button

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -3,7 +3,28 @@ import React, { useState } from "react";
 function TodoForm() {
   const [input, setInput] = useState("");
 
-  return <div>TodoForm</div>;
+  return (
+    <div>
+      <div class="input-group mb-3">
+        <input
+          type="text"
+          class="form-control"
+          aria-label="Recipient's username"
+          aria-describedby="button-addon2"
+          value={input}
+          placeholder="Add a task"
+          name="text"
+        />
+        <button
+          class="btn btn-outline-secondary"
+          type="button"
+          id="button-addon2"
+        >
+          Add
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default TodoForm;
