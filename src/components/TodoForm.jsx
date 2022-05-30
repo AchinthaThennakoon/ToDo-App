b9c2c9fbd6006899bfb308d1c0c10a3e4d0d389e
achintha <achinthathennakoon97@gmail.com>
2022-05-30 15:52:23 +0530

    set use state

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,6 +1,8 @@
-import React, { userState } from "react";
+import React, { useState } from "react";
 
 function TodoForm() {
+  const [input, setInput] = useState("");
+
   return <div>TodoForm</div>;
 }
 
